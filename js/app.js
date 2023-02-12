cca55a69ca26b8a5a8ac654137f383f4d713b3b4
imaadcelman@gmail.com <imaadcelman@gmail.com>
2023-02-12 17:56:04 +0530

    modified the styles files

--- js/app.js
+++ js/app.js
@@ -1,39 +1,3 @@
-/*
-
-
-An Immediately Invoked Function Expression (IIFE) in JavaScript is defined as follows:
-
-(function () {
-  // code to be executed immediately
-})();
-
-The function expression is wrapped in parentheses () to indicate that it's an expression, and the parentheses () at the end of the expression indicate that the function should be immediately invoked. The function is anonymous, and its result is not saved in a variable, which makes it suitable for one-time use cases such as setting up a local scope for variables, encapsulating code, or creating a closure.
-*/
-/* ---------------------------------------------------------------------------------- */
-/* ---------------------------------------------------------------------------------- */
-/* ---------------------------------------------------------------------------------- */
-
-/*
-                  -------------------------EXPLANATION OF THE BELOW JAVASCRIPT code---------------------------
-                  
-This code defines an anonymous function and immediately runs it.
-
-The given code is an immediately invoked function expression (IIFE) in JavaScript. It's a function that's executed immediately after it's defined. The purpose of using an IIFE is to create a private scope for the function, so that the variables and functions defined inside it are not accessible from outside the function.
-
-The code is setting up event listeners for buttons with the class "control". When these buttons are clicked, the code removes the "active-btn" class from the currently active button and adds it to the button that was just clicked. The code also removes the "active" class from the currently active element and adds it to the element associated with the button that was just clicked (determined by the "dataset.id" property of the button).
-
-The code is also setting up an event listener for a button with the class "theme-btn". When this button is clicked, the code toggles the "light-mode" class on the document body.
-
-The function is called at the end of the code by adding () after the definition of the function, causing it to be invoked immediately.
-
-(function () {
-  ...
-})();
-
-This is called an immediately invoked function expression (IIFE). The function is defined and executed immediately, so there is no need for a separate function call.
-
-*/
-
 (function () {
   [...document.querySelectorAll(".control")].forEach((button) => {
     button.addEventListener("click", function () {
