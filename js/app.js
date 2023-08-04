0f59f2841b8f597e482482fbac982a84491a3bf2
imaadcelman@gmail.com <imaadcelman@gmail.com>
2023-08-04 22:52:17 +0530

    new afresh look and updated resume

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+// Function to close the popup
+function closePopup() {
+  document.getElementById("popup-overlay").style.display = "none";
+}
+
+// Function to show the popup when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+  var popupOverlay = document.getElementById("popup-overlay");
+  var popupMessage = document.getElementById("popup-message");
+  var isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  if (isMobile) {
+    popupMessage.innerHTML =
+      "<i class='fa-solid fa-lightbulb fa-beat'></i>&ensp;<b>Did you know?</b><br>This portfolio will also look great on <b>desktop.</b>&emsp;<button id='ok-button' onclick='closePopup()'> <i class='fa-solid fa-check fa-xl'></i></button>";
+  } else {
+    popupMessage.innerHTML =
+      "<i class='fa-solid fa-lightbulb fa-beat'></i>&ensp;<b>Did you know?</b><br>This portfolio will also look great on <b>mobile.</b>&emsp;<button id='ok-button' onclick='closePopup()'> <i class='fa-solid fa-check fa-xl'></i> </button>";
+  }
+
+  // Display the popup after a delay (e.g., 3 seconds)
+  setTimeout(function () {
+    popupOverlay.style.display = "block";
+  }, 2500); // Adjust the delay (in milliseconds) as needed
+});
+
+// Function to close the popup when clicking outside of it
+window.addEventListener("click", function (event) {
+  var popupOverlay = document.getElementById("popup-overlay");
+  if (event.target === popupOverlay) {
+    closePopup();
+  }
+});
+
 // Immediately Invoked Function Expression (IIFE)
 (function () {
   [...document.querySelectorAll(".control")].forEach((button) => {
@@ -12,34 +48,6 @@
     document.body.classList.toggle("light-mode");
   });
 })();
-
-//for #matrix
-// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// let interval = null;
-// document.getElementById("matrix").onmouseover = (event) => {
-//   let iteration = 0;
-
-//   clearInterval(interval);
-
-//   interval = setInterval(() => {
-//     event.target.innerText = event.target.innerText
-//       .split("")
-//       .map((letter, index) => {
-//         if (index < iteration) {
-//           return event.target.dataset.value[index];
-//         }
-
-//         return letters[Math.floor(Math.random() * 26)];
-//       })
-//       .join("");
-
-//     if (iteration >= event.target.dataset.value.length) {
-//       clearInterval(interval);
-//     }
-
-//     iteration += 1 / 6;
-//   }, 30);
-// };
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let interval = null;
